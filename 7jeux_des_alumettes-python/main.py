919f2802a200fb3d606b14a2a2636cddef7713a5
Elsa Catoire <elsa.catoire@gmail.com>
2023-03-29 15:29:33 +0200

    modif de ask en récursive

--- 7jeux_des_alumettes-python/main.py
+++ 7jeux_des_alumettes-python/main.py
@@ -10,7 +10,7 @@ candy_box = 20
 
 def remove_candies(candy_nb):
     """
-    removes the given number of candies from the total amount
+    removes the user's given number of candies from the total amount
     :param candy_nb: candies' quantity that the player chooses to remove
     :return: quantity of candies left
     """
@@ -23,15 +23,14 @@ def ask_quantity(x):
     """
     ask one player a quantity to remove from the box
     quantity has to be between 1 and 6
-
-
-    :return:
+    :param: x : quantity of candy to withdraw
+    :return: a number, quan
     """
-    player_quantity_str = "a"
-    while player_quantity_str not in ["1", "2", "3", "4", "5", "6"]:
-        player_quantity_str = input("Hi ! How many candy do you take ?")
-        print("number has to be between 1 and 6")
+    player_quantity_str = input("Hi ! How many candy do you take ?")
     player_quantity_int = int(player_quantity_str)
+    if player_quantity_str not in ["1", "2", "3", "4", "5", "6"]:
+        print("number has to be between 1 and 6")
+    ask_quantity(x)
     return player_quantity_int
 
 
@@ -66,7 +65,7 @@ def ask_number_of_players():
 
     :return: number of players
     """
-    number_players = int(input("How may players ?"))
+    number_players = int(input("How many players ?"))
     return number_players
 
 
