d5a7b247f2cebb1f00beb4a0c7f00a726179213f
Elsa Catoire <elsa.catoire@gmail.com>
2023-04-20 15:10:36 +0200

    function describe ok

--- 11conway-python/main.py
+++ 11conway-python/main.py
@@ -19,9 +19,30 @@ def cut_string(string_to_cut):
         else:
             new_string = new_string + char
     new_string = new_string + string_to_cut[-1]
-    print(new_string)
+    return new_string
+
+
+def describe_string(string_to_describe):
+    """
+    to detail a string as a code
+    :param string_to_describe: what u want to describe
+    :return: the description
+    """
+    new_string = ""
+    count = 1
+    for i in range(len(string_to_describe) - 1):
+        char = string_to_describe[i]
+        char2 = string_to_describe[i+1]
+        if char != char2:
+            new_string = new_string + str(count) + char
+            count = 1
+        else:
+            count += 1
+    new_string = new_string + str(count) + string_to_describe[-1]
+    return new_string
 
 
 if __name__ == '__main__':
-    cut_string("coolleraghjjto")
+    print(cut_string("aabbca"), "/expected : aa bb c a")
+    print(describe_string("aabbca"), "/expected 2a2b1c1a")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
