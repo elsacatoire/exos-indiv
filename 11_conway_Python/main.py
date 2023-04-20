0159bc313f2adc600f7ce3ef7ec6b6c07083c525
Elsa Catoire <elsa.catoire@gmail.com>
2023-04-20 10:22:11 +0200

    first commit set files

---
+++ 11_conway_Python/main.py
@@ -0,0 +1,10 @@
+"""
+Exercise 11 : Conway
+Elsa Catoire - 20.04.2023
+"""
+
+
+if __name__ == '__main__':
+    # given_quantity = ask_quantity()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
