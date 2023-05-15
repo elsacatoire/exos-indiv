6e67e736d2591f72dc50556153fcd22520dcb1eb
Elsa Catoire <elsa.catoire@gmail.com>
2023-05-15 16:49:56 +0200

    new file to try Object programming

---
+++ 13poker-python/main2.py
@@ -0,0 +1,30 @@
+import random
+
+
+class Carte:
+    """Standard card"""
+    def __init__(self, color=0, value=2):
+        self.color = color
+        self.value = value
+
+    color_names = ["Spade", "Club", "Heart", "Diamond"]
+    value_names =
+
+
+def create_deck():
+    """
+    Create a standard deck of card, unordered
+    :return: the deck
+    """
+    new_deck = 52
+
+    return new_deck
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("hello")
+    print(create_deck())
+
