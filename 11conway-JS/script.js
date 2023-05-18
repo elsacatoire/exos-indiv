af5ef1fc3ff892b2e6ae077ac686aa2485bf0d54
Elsa Catoire <elsa.catoire@gmail.com>
2023-05-18 22:45:17 +0200

    conway suit ok avec log

--- 11conway-JS/script.js
+++ 11conway-JS/script.js
@@ -28,9 +28,33 @@ function describeStr(stringToDescribe) {
     return finalStr
 }
 
+function conwaySuit(char, n) {
+    let transitionStr = char
+    let resultConway = [char]
+    for (let i = 0; i < n - 1; i++) {
+        transitionStr = describeStr(transitionStr)
+        resultConway.push(transitionStr)
+    }
+    return resultConway
+}
+
+function conwaySuitLog(char, n) {
+    let transitionStr = char
+    for (let i = 0; i < n; i++) {
+        console.log(transitionStr)
+        transitionStr = describeStr(transitionStr)
+    }
+    return
+}
+
 
 // main application
 let result1 = cutString(simpleStr)
 let result2 = cutString(biggerStr)
 let resultDescribe1 = describeStr(simpleStr)
 let resultDescribe2 = describeStr(biggerStr)
+let testConway1 = conwaySuit('a', 3)
+let testConway2 = conwaySuit('1', 3)
+console.log(testConway1);
+console.log(testConway2);
+conwaySuitLog('a', 3)
