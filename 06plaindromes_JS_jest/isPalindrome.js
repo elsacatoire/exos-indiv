d7a7db229310c434f240c08b3c6025e40d82deda
Elsa Catoire <elsa.catoire@gmail.com>
2023-05-19 17:26:25 +0200

    add and past test for invalid date in isPalindrome

--- 06plaindromes_JS_jest/isPalindrome.js
+++ 06plaindromes_JS_jest/isPalindrome.js
@@ -1,8 +1,13 @@
+const isValidDate = require('./isValidDate')
+
 function isPalindrome(dateToTry) {
-    let joinDate = dateToTry.replaceAll('/', '')
-    let reverseJoinedDate = joinDate.split('').reverse().join('')
-    if (joinDate == reverseJoinedDate) {
-        return true
+    if (isValidDate(dateToTry)) {
+        let joinDate = dateToTry.replaceAll('/', '')
+        let reverseJoinedDate = joinDate.split('').reverse().join('')
+        if (joinDate == reverseJoinedDate) {
+            return true
+        }
+        return false
     }
     return false
 }
--- 06plaindromes_JS_jest/isPalindrome.test.js
+++ 06plaindromes_JS_jest/isPalindrome.test.js
@@ -9,3 +9,7 @@ test('parlindrome yes', () => {
     const result = isPalindrome("03/04/2001")
     expect(result).toBeFalsy()
 })
+test('parlindrome invalid date', () => {
+    const result = isPalindrome("03/14/2001")
+    expect(result).toBeFalsy()
+})
