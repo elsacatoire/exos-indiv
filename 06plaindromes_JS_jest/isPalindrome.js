ad4714c5579372a6db5f2e74dacfaf7588d8f4fb
Elsa Catoire <elsa.catoire@gmail.com>
2023-05-20 18:58:01 +0200

    optimized isPalindrome

--- 06plaindromes_JS_jest/isPalindrome.js
+++ 06plaindromes_JS_jest/isPalindrome.js
@@ -2,14 +2,11 @@ const isValidDate = require('./isValidDate')
 
 function isPalindrome(dateToTry) {
     if (isValidDate(dateToTry)) {
-        let joinDate = dateToTry.replaceAll('/', '')
-        let reverseJoinedDate = joinDate.split('').reverse().join('')
-        if (joinDate == reverseJoinedDate) {
-            return true
-        }
-        return false
+        let joinDate = dateToTry.replaceAll('/', '');
+        let reverseJoinedDate = joinDate.split('').reverse().join('');
+        return joinDate === reverseJoinedDate;
     }
-    return false
+    return false;
 }
 
 module.exports = isPalindrome;
