5a0106603e5f0697d416a231e82c25ca9b6a41d0
Elsa Catoire <elsa.catoire@gmail.com>
2023-05-20 18:55:35 +0200

    optimized isValidDate function

--- 06plaindromes_JS_jest/isValidDate.js
+++ 06plaindromes_JS_jest/isValidDate.js
@@ -1,26 +1,29 @@
 const maxDaysInMonth = require('./maxDaysInMonth')
 
 function isValidDate(dateToTest) {
-    const regex = /^\d{2}\/\d{2}\/\d{4}$/
-    if (isValid = regex.test(dateToTest)) {
-        dateParts = dateToTest.split('/')
-        dayInt = parseInt(dateParts[0], 10)
-        monthInt = parseInt(dateParts[1], 10)
-        yearInt = parseInt(dateParts[2], 10)
-        if (monthInt > 0 && monthInt <= 12) {
-            if (dayInt > 0 && dayInt < 31) {
-                if (maxDaysInMonth(monthInt, dayInt)) {
-                    if (yearInt > 999 && yearInt < 9999) {
-                        return true
-                    }
-                    return false
-                }
-                return false
-            }
-            return false
-        }
-        return false
+    const regex = /^\d{2}\/\d{2}\/\d{4}$/;
+    if (!regex.test(dateToTest)) {
+        return false;
     }
+    const dateParts = dateToTest.split('/');
+    const dayInt = parseInt(dateParts[0], 10);
+    const monthInt = parseInt(dateParts[1], 10);
+    const yearInt = parseInt(dateParts[2], 10);
+
+    if (monthInt <= 0 || monthInt > 12) {
+        return false;
+    }
+    if (dayInt <= 0 || dayInt > 31) {
+        return false;
+    }
+    if (!maxDaysInMonth(monthInt, dayInt)) {
+        return false;
+    }
+    if (yearInt <= 999 || yearInt >= 9999) {
+        return false;
+    }
+    return true;
 }
+
 
 module.exports = isValidDate
