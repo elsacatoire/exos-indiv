0187f3ace353588cf02a98793741e6d4b394c2af
Elsa Catoire <elsa.catoire@gmail.com>
2023-05-20 19:00:50 +0200

    optimized maxDays

--- 06plaindromes_JS_jest/maxDaysInMonth.js
+++ 06plaindromes_JS_jest/maxDaysInMonth.js
@@ -1,13 +1,11 @@
 function maxDaysInMonth(month, day) {
-    if (month == 2) {
-        return day <= 28
+    if (month === 2) {
+        return day <= 28;
     }
-    if (month == 4 || month == 6 || month == 9 || month == 11) {
-        return day <= 30
-
-    } else {
-        return day <= 31
+    if (month === 4 || month === 6 || month === 9 || month === 11) {
+        return day <= 30;
     }
+    return day <= 31;
 }
 
 module.exports = maxDaysInMonth
