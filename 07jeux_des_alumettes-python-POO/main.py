0d0223f69a2a8b5b393895e9174e93be45412958
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-16 10:53:28 +0200

    ajout game play pour remove

--- 07jeux_des_alumettes-python-POO/main.py
+++ 07jeux_des_alumettes-python-POO/main.py
@@ -6,17 +6,24 @@ POO
 
 
 class MatchBox:
+    """
+    Administration of the matches box we play with.
+    """
     def __init__(self, matches_quantity):
         self.quantity = matches_quantity
 
-    def _get_quantity(self):
+    def get_quantity(self):
         try:
             return print(self.quantity)
         except AttributeError:
             print("quantity is not defined")
 
-    def remove_matches(self, quantity_to_remove):
-        self.quantity -= quantity_to_remove
+    def remove_matches(self, quantity_to_remove, player):
+        if quantity_to_remove in [1, 2, 3, 4, 5, 6]:
+            print(f"{player.name} removes {quantity_to_remove} matches")
+            self.quantity -= quantity_to_remove
+        else:
+            print("quantity should be between 1 and 6")
 
     def did_win_the_game(self):
         if self.quantity <= 0:
@@ -26,13 +33,15 @@ class MatchBox:
 
 
 class Player:
+    """
+    Everything to create a player.
+    """
     def __init__(self, name, is_winner):
         self.name = name
         self.is_winner = is_winner
 
-    def _get_is_winner(self):
-        if self.is_winner == True:
-
+    def get_is_winner(self):
+        return print(self.is_winner)
 
     def win(self):
         self.is_winner = True
@@ -42,11 +51,11 @@ class Player:
 if __name__ == '__main__':
     # given_quantity = ask_quantity()
     match_box = MatchBox(50)
-    match_box.remove_matches(3)
-    match_box._get_quantity()
+    match_box.get_quantity()
     player1 = Player("Elsa", False)
     player2 = Player("Alissia", False)
-    player1.i
+    match_box.remove_matches(3, player1)
+    player1.get_is_winner()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
