670bbb2a4bd49127ab04699c1d8f2aa8c4eaad9c
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-16 15:35:36 +0200

    initialize Deck implémenté dans deck

--- 13poker-python/poker/src/Deck.java
+++ 13poker-python/poker/src/Deck.java
@@ -8,6 +8,14 @@ public class Deck {
     }
 
     public void initializeDeck() {
+        String[] suits = {"Heart", "Diamonds", "Clubs", "Spades"};
+        String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
+        for (String suit : suits) {
+            for (String value : values){
+                Card card = new Card(value, suit);
+                cards.add(card);
+            }
+        }
     }
 }
