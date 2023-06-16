2dcadc6743808b0e5e96b20a81efdf4059fc2bde
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-16 15:27:18 +0200

    getter des attributs de Cards

--- 13poker-python/poker/src/Card.java
+++ 13poker-python/poker/src/Card.java
@@ -1,6 +1,14 @@
 public class Card {
     private String value;
     private String suit;
+
+    public String getValue(){
+        return value;
+    }
+
+    public String getSuit(){
+        return suit;
+    }
     public Card(String  value, String suit){
         this.value = value;
         this.suit = suit;
