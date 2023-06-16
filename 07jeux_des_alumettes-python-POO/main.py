f708dbc03e217ef8ac1ecf9769d995e5edd295ec
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-16 11:54:49 +0200

    ajout ask quantity et refactor remove

--- 07jeux_des_alumettes-python-POO/main.py
+++ 07jeux_des_alumettes-python-POO/main.py
@@ -10,26 +10,25 @@ class MatchBox:
     Administration of the matches box we play with.
     """
     def __init__(self, matches_quantity):
-        self.quantity = matches_quantity
+        self._quantity = matches_quantity
 
-    def get_quantity(self):
+    def _get_total_matches(self):
         try:
-            return print(self.quantity)
+            return self.quantity
         except AttributeError:
             print("quantity is not defined")
 
     def remove_matches(self, quantity_to_remove, player):
-        if quantity_to_remove in [1, 2, 3, 4, 5, 6]:
-            print(f"{player.name} removes {quantity_to_remove} matches")
-            self.quantity -= quantity_to_remove
-        else:
-            print("quantity should be between 1 and 6")
+        print(f"{player.name} removes {quantity_to_remove} matches")
+        self.quantity -= quantity_to_remove
 
     def did_win_the_game(self):
         if self.quantity <= 0:
             print("You won the game")
+            return True
         else:
             print("Next Player")
+            return False
 
 
 class Player:
@@ -37,14 +36,48 @@ class Player:
     Everything to create a player.
     """
     def __init__(self, name, is_winner):
-        self.name = name
-        self.is_winner = is_winner
+        self._name = name
+        self._is_winner = is_winner
 
-    def get_is_winner(self):
-        return print(self.is_winner)
+    def ask_quantity(self):
+        given_quantity = 0
+        while given_quantity not in [1, 2, 3, 4, 5, 6]:
+            given_quantity = input(f"{self.name} how many matches do you take ?")
+        return int(given_quantity)
+
+    def _get_name(self):
+        try:
+            return self._name
+        except AttributeError:
+            print("Error : name is not defined")
+
+    def _get_is_winner(self):
+        return self._is_winner
 
     def win(self):
-        self.is_winner = True
+        self._is_winner = True
+
+
+def end_game():
+    print("game is over")
+
+
+class Game:
+    def __init__(self, game_nb):
+        self.round = game_nb
+
+    # initialize the game
+    match_box = MatchBox(50)
+    quantity_of_players = int(input("How many players ?"))
+    for player in quantity_of_players:
+        player_name = input(f"Hello player {player}, what is your name ?")
+        player1 = Player(player_name, False)
+
+    # play the game
+    while match_box._quantity > 50:
+        quantity_played = player1.ask_quantity()
+        match_box.remove_matches(quantity_played)
+
 
 
 
