14921c27ad3f5d8a835a3155aa9801eefd2f8f9f
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-16 09:58:31 +0200

    premier commit

---
+++ 07jeux_des_alumettes-python-POO/main.py
@@ -0,0 +1,16 @@
+"""
+Exercise 7 : Candies' game
+Elsa des bois 16 06 2023
+POO
+"""
+
+
+def hello(name):
+    print(f"hello {name}")
+
+
+if __name__ == '__main__':
+    # given_quantity = ask_quantity()
+    hello("Elsa")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
